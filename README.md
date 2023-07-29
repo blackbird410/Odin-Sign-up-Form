0a11ca5ced191b8561ad1e73c765af000a865119
blackbird410 <ntsrigaud14@gmail.com>
2023-07-30 00:27:07 +0800

    Modified UI

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ This is a repository for a form webpage project, part of the Intermediate HTML a
 
 # Image Credits
 Photo by <a href="https://unsplash.com/@haliewestphoto?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Halie West</a> on <a href="https://unsplash.com/@haliewestphoto?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+Photo by <a href="https://unsplash.com/@neom?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">NEOM</a> on <a href="https://unsplash.com/photos/bhKqZNZeAR0?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
   
--- code.js
+++ code.js
@@ -1,3 +1,4 @@
+const body = document.body;
 const form = document.querySelector("form");
 const fName = document.getElementById('first-name');
 const lName = document.getElementById('last-name');
@@ -15,29 +16,91 @@ const cPswrdError = document.getElementById('cPswrdError');
 
 const reName = /^([A-Z][a-z]{1,20})([\s-]+[A-Za-z]{2,20}(-[A-Za-z]{2,20})?)?$/gm
 
+let fNameValid = false;
+let lNameValid = false;
+let emailValid = false;
+let phoneValid = false;
+let passCorrect = false;
+let passwordValid = false;
+
+const cover = document.createElement('div');
+const message = document.createElement('div');
+const imgCredit = document.createElement('div');
+const link1 = document.createElement('a');
+const link2 = document.createElement('a');
+
+// ------------------ Defining a thanks cover------------------------
+
+cover.className = 'cover';
+message.className = 'message';
+imgCredit.className = 'img-credit';
+link1.href = "https://unsplash.com/@neom?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText";
+link2.href = "https://unsplash.com/photos/bhKqZNZeAR0?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText";
+link1.textContent = " NEOM on";
+link2.textContent = " Unsplash";
+
+message.textContent = "Thank You for Signing Up! Let the adventure begin!";
+imgCredit.textContent = "Photo by";
+imgCredit.appendChild(link1);
+imgCredit.appendChild(link2);
+cover.appendChild(message);
+cover.appendChild(imgCredit);
+body.appendChild(cover);
+// --------------------------------------------------------------------------------------------
+
 fName.addEventListener('input', () => {
-    fNameError.textContent = fName.value.match(reName)? "" : "Input invalid!";
+    if (fName.value.match(reName)) {
+        fNameError.textContent = "";
+        fNameValid = true;
+    }
+    else {
+        fNameError.textContent = "Invalid input";
+        fNameValid = true;
+    }
 });
 
 lName.addEventListener('input', () => {
-    lNameError.textContent = lName.value.match(reName)? "" : "Invalid input";
+    if (lName.value.match(reName)) {
+        lNameError.textContent = "";
+        lNameValid = true;    
+    } else {
+        lNameError.textContent = "Invalid input";
+        lNameValid = true;
+    }
 });
 
 email.addEventListener('input', () => {
-    emailError.textContent = email.value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)? "" : "Invalid email";
+    if (email.value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
+        emailError.textContent = "";
+        emailValid = true;
+    } else {
+        emailError.textContent = "Invalid email";
+        emailValid = false;   
+    }
 });
 
 phone.addEventListener('input', () => {
-    phoneError.textContent = phone.value.match(/^([+]\d{1,3})?\d{8,10}$/)? "" : "Invalid number";
+    if (phone.value.match(/^([+]\d{1,3})?\d{8,10}$/)) {
+        phoneError.textContent = "";
+        phoneValid = true;
+    } else {
+        phoneError.textContent = "Invalid number";
+        phoneValid = false;   
+    }
 });
 
 pswrd.addEventListener('input', () => {
-    const p = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z0-9]{8,20}$/
-    pswrdError.textContent = pswrd.value.match(p)? "" : "Invalid Password";
-    console.log(pswrd.value);
+    const p = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z0-9]{8,20}$/;
+    if (pswrd.value.match(p)) {
+        pswrdError.textContent = "";
+        passCorrect = true;
+    } else {
+        pswrdError.textContent = "Invalid Password";
+        passCorrect = false;    
+    }
+    
 });
 
-let passwordValid = false;
 cPswrd.addEventListener('input', () => {
     if(cPswrd.value !== pswrd.value) {
         cPswrdError.textContent = "Not matched"
@@ -48,6 +111,8 @@ cPswrd.addEventListener('input', () => {
         passwordValid = true;
     }
 });
+
+let b = false;
 
 form.addEventListener("submit", (event) => {
     Array.from(document.querySelectorAll('input')).forEach(elt => {
@@ -60,7 +125,12 @@ form.addEventListener("submit", (event) => {
         }
     });
 
-    if(!passwordValid) {
+    b = lNameValid && fNameValid && emailValid && phoneValid && passCorrect && passwordValid;
+    
+    if (!b) {
         event.preventDefault();
+    }
+    else {
+        cover.classList.add('visible');
     }
 });
